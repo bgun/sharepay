8abf3326c68ea9bbe284b8ba38460374f20e1905
Ben Gundersen <ben@bengundersen.com>
2013-11-09 09:56:51 -0500

    Setting up procfile for Heroku

---
+++ web.js
@@ -0,0 +1,32 @@
+var _       = require("underscore");
+var fs      = require("fs");
+var express = require("express");
+var mongoos = require("mongoose");
+
+var app = express();
+
+function getStaticFile(filename, callback) {
+  fs.readFile("public/"+filename,function(err,f) {
+    if(err) {
+      throw err;
+    }
+    callback.call(null,f);
+  });
+}
+
+console.log("Running server from "+__dirname);
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(8080);
+
+app.get("/",function(req, res) {
+  res.send("<h1>SharePay</h1>");
+});
+app.get("/test",function(req, res) {
+  //console.log(req);
+  getStaticFile("anim.gif",function(f) {
+    res.set("Content-Type","image/gif");
+    res.send(f);
+  });
+});

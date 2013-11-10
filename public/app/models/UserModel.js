1216bd75d43710a326d840becbd9dc58a943c24e
Pavel Shub <pavel.987@gmail.com>
2013-11-09 22:28:09 -0500

    usermodel template

---
+++ public/app/models/UserModel.js
@@ -0,0 +1,18 @@
+App.Models.UserModel = Backbone.Model.extend({
+	
+	defaults: {
+	},
+	
+	initialize: function(options) {
+		var that = this;
+		/*vendorAjax = $.ajax({
+			type: 'GET',
+			url: url + '/api/user',
+			dataType: "json"
+		}).done(function(data){
+			
+		});
+		*/
+	}
+
+});

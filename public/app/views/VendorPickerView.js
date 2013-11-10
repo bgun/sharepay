86cc56000d2dd357baa95e1ba711f2d8918d2852
Gregory Gundersen <ggundersen@gmail.com>
2013-11-09 21:05:33 -0500

    nested vendor temp

--- public/app/views/VendorPickerView.js
+++ public/app/views/VendorPickerView.js
@@ -6,20 +6,32 @@ App.Views.VendorPickerView = Backbone.View.extend({
 
 	initialize: function(options) {
 		this.options = options;
+		this.parentEl = this.options.parentView.$el;
 	},
 
+	// `render` renders the HTML container, `vendor-list-container`.
 	render: function() {
 		var that = this,
-			templateFn = _.template(templateManager.getTemplate('vendor'));
+			templateFn = _.template(templateManager.getTemplate('vendors'));
 
-    	_.each(this.collection.models, function(vendorView) {
-    		var obj = vendorView.toJSON(),
-    			html = templateFn(obj);
-
-			that.options.parentView.$el.append(html);
+		var html = templateFn({
+			vendors: this.collection.models
 		});
 
-		return this;
+		this.parentEl.append(html);
+	},
+
+	// `renderVendors` renders the individual models in the collection.
+	renderVendors: function() {
+		/*var that = this,
+			templateFn = _.template(templateManager.getTemplate('vendor'));
+
+		_.each(this.collection.models, function(vendorView) {
+			var obj = vendorView.toJSON(),
+				html = templateFn(obj);
+
+			that.parentEl.append(html);
+		});*/
 	}
 
 });

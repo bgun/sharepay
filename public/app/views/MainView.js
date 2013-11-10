3cb5bad4a8d499121f3ea58ededf9d3128faf0ac
Gregory Gundersen <ggundersen@gmail.com>
2013-11-09 20:16:32 -0500

    working on getting real data

--- public/app/views/MainView.js
+++ public/app/views/MainView.js
@@ -9,15 +9,28 @@ App.Views.MainView = Backbone.View.extend({
 	},
 
 	initialize: function(options) {
-		this.childViews = [];
-		this.childViews.push(new App.Views.VendorPickerView({
-			parentView: this,
-			collection: App.vendors
-		}));
-		this.childViews.push(new App.Views.ProcessorsPickerView({
-			parentView: this,
-			collection: App.processors
-		}));
+		var url = 'http://sharepay.herokuapp.com';
+
+		var vendorAjax = $.ajax({
+			url: url + '/api/vendors'
+		});
+		
+		var processorAjax = $.ajax({
+			url: url + '/api/processors'
+		});
+
+		$.when(vendorAjax, processorAjax).done(function(vendors, processors) {
+			console.log(vendors, processors);
+			this.childViews = [];
+			this.childViews.push(new App.Views.VendorPickerView({
+				parentView: this,
+				collection: vendors
+			}));
+			this.childViews.push(new App.Views.ProcessorsPickerView({
+				parentView: this,
+				collection: processors
+			}));
+		});		
 	},
 
 	render: function() {

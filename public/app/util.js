8c70b27e1509b5b3534f78c6393c735c44fcec68
Micah Freedman <i@micahfreedman.com>
2013-11-10 00:15:05 -0500

    fixing Utils.arraySum

--- public/app/util.js
+++ public/app/util.js
@@ -2,8 +2,10 @@ App.module("Utils", function(Mod, App, Backbone, Marionette, $, _) {
   Mod.isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
   };
-  Mod.arraySum = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+  Mod.arraySum = function(arr) {
+    return _(arr).reduce(function(m,x) {
+      return m + x; 
+    }, 0);
   };
   Mod.hashSum = function(hash, param) {
     return this.arraySum(_(hash).pluck(param));

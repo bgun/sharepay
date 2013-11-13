2596e208b37882c1b92f2487927adfd9bb1d30d4
Ben Gundersen <ben@citymaps.com>
2013-11-13 10:49:09 -0500

    Added grunt and LESS

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        // Compile all targeted LESS files individually
+        options: {
+          compress: true
+        },
+        // target name
+        files: {
+          "public/css/vendors.css" : "public/css/vendors.less",
+          "public/css/styles.css"  : "public/css/styles.less",
+          "public/css/cart.css"    : "public/css/cart.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        // Which files to watch
+        files: ['public/css/*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['watch']);
+
+};
